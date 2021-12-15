17b80e6076b49281e45f8c9555e8422e1ad7d9dc
Julien de Fondaumière <deji97410@gmail.com>
2021-12-15 23:03:03 +0100

    .PNG to .png

--- js/product.js
+++ js/product.js
@@ -1,12 +1,14 @@
-const id = new URL(window.location.href).searchParams.get('id');
+const id = new URL(window.location.href).searchParams.get('id')
+const addToCartBtn = document.getElementById('addToCart')
+// let localStorage = Window.localStorage
 fetch('http://localhost:3000/api/products/'+ id)
     .then(response => response.json())
     .then(product => {
         // ADD the image
         let imgItemEl = document.getElementsByClassName('item__img');
         let img = document.createElement('img')
-        img.src = product.imageUrl;
-        imgItemEl[0].appendChild(img);
+        img.src = product.imageUrl
+        imgItemEl[0].appendChild(img)
 
         // ADD the name
         let titleEl = document.getElementById('title')
@@ -34,4 +36,17 @@ fetch('http://localhost:3000/api/products/'+ id)
             newOption.appendChild(colorText)
             selectEl.appendChild(newOption)
         }
+
+        let quant = document.getElementById('quantity')
+        
+        // ADD the add to cart button listener
+        addToCartBtn.onclick = function(){
+            let dataArray = ["", "", ""]
+            dataArray[0] = product._id
+            dataArray[1] = selectEl.options[selectEl.selectedIndex].text
+            dataArray[2] = quant.value
+            console.log(dataArray)
+            localStorage.setItem("dataArray", JSON.stringify(dataArray))
+        }
 });
+
