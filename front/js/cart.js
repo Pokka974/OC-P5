2bdb0b9f58c71f5dba6824582136f44c0f3cd5d5
Julien de Fondaumière <deji97410@gmail.com>
2021-12-21 04:53:49 +0100

    done doing the update and delete item from cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -50,6 +50,7 @@ for(let i of storage){
         p1.textContent = i[1]
         
         let p2 = document.createElement("p")
+        p2.className = 'item__price'
         p2.textContent = product.price + " €"
 
         //ADD content settings
@@ -91,45 +92,145 @@ for(let i of storage){
 }
 
 var total = document.getElementById('totalQuantity')
-total.textContent = totalQuantity
+
+total.textContent = totalQuantity === 0 ? 0 : totalQuantity
 
 
 
 window.addEventListener('load', function (){
 
     let allInputs = document.getElementsByClassName("itemQuantity")
-    console.log(allInputs)
     for(let i of allInputs){
         i.addEventListener('change', (e) => {
             console.log(e.target.valueAsNumber)
 
-            //Updating the quantity in view
+            //Updating the quantity in view and localStorage
             updateQuantity(i)
         })
     }
+
+    let allDeleteBtn = document.getElementsByClassName("deleteItem")
+    // deleteItem(allDeleteBtn[0])
+    for(let j of allDeleteBtn){
+        j.addEventListener('click', () => {
+            deleteItem(j)
+        })
+    }
+
+    updateTotalPrice()
 })
 
 function updateQuantity(i){
-
+    let promise1;
     let quant = i.closest(".cart__item__content__settings__quantity").firstElementChild
 
-    //Updating the total amount
-    let updateNumb = i.valueAsNumber - parseInt(quant.textContent.split(' ')[2])
-    totalQuantity += updateNumb
-    total.textContent = totalQuantity
+    let inputIsEmpty = i.value.toString().trim().length == 0 ? true : false
     
     //Get the id and color of selected item
     let id = i.closest('.cart__item').dataset.id
     let color = i.closest('.cart__item').dataset.color
-    quant.textContent = "Qté : " + i.valueAsNumber
 
+    if(!inputIsEmpty){
+        quant.textContent = "Qté : " + i.valueAsNumber
+    }
+    
     //updating the localstorage
     for(let l = 0; l < localStorage.length; l++){
         let loc = JSON.parse(localStorage.getItem(localStorage.key(l)))
         loc.splice(2, 1, i.valueAsNumber)
         if(loc[0] == id && loc[1] == color){
-            localStorage.setItem(localStorage.key(l), JSON.stringify(loc))
+            promise1 = new Promise((resolve, reject) => {
+                resolve(localStorage.setItem(localStorage.key(l), JSON.stringify(loc)))
+            })
             break;
         }
     }
+
+    promise1.then((value) => {
+        updateTotalQuantity()
+        updateTotalPrice()
+    })
+    
+}
+
+function deleteItem(i){
+    if(confirm('Voulez vous vraiment supprimer article du panier ?')){
+        // cart__item
+        let promise2
+        let quant = i.closest('.cart__item__content__settings').firstElementChild.children[0]
+        let found = false
+        //GET the color and the name of the corresponding item
+        let bloc = i.closest('.cart__item')
+        let bloc_description = i.closest('.cart__item__content').firstElementChild
+        let name = bloc_description.children[0]
+        let color = bloc_description.children[1]
+        console.log('Click on item ' + name.textContent + ' ' + color.textContent)
+
+        //find it in the localStorage and remove
+        for(let i = 0 ; i < localStorage.length; i++){
+            console.log(localStorage.key(i))
+            if(localStorage.key(i) == name.textContent + ' ' + color.textContent){
+                console.log('found one !')
+                localStorage.removeItem(localStorage.key(i))
+                found = true
+            }
+        }
+
+        //delete the HTML element in the DOM
+        if(found){
+            promise2 = new Promise((resolve, reject) => {
+                resolve(bloc.remove())
+            })
+        }
+            
+        promise2.then((value) =>{
+            updateTotalQuantity()
+            updateTotalPrice()
+        })
+    }
+    
+}
+
+function updateTotalPrice(){
+
+    let totalPriceElement = document.getElementById('totalPrice')
+   
+    let totalPrice = 0
+    let totalQuant = []
+    let totalPrices = []
+    let allPricesEl = document.getElementsByClassName('item__price')
+    let allQuantEl = document.getElementsByClassName('cart__item__content__settings__quantity')
+
+    for(let i of allQuantEl){
+        totalQuant.push(i.firstChild.textContent.split(' ')[2])
+    }
+
+    console.log(allPricesEl)
+    for(let j of allPricesEl){
+        totalPrices.push(parseInt(j.textContent.split(' ')[0]))
+    }
+
+    for(let k = 0; k < totalQuant.length; k++){
+        let x = totalPrices[k] * totalQuant[k]
+        totalPrice += x
+    }
+    console.log(totalPrice)
+
+    totalPriceElement.textContent = totalPrice
+}
+
+function updateTotalQuantity(){
+    let totalQuantityElement = document.getElementById('totalQuantity')
+
+    let totalQuantity = 0
+    let allQuantEl = document.getElementsByClassName('cart__item__content__settings__quantity')
+
+    for(let i of allQuantEl){
+        console.log(i.firstChild.textContent.split(' ')[2])
+        totalQuantity += parseInt(i.firstChild.textContent.split(' ')[2])
+    }
+
+    console.log(totalQuantity)
+
+    totalQuantityElement.textContent = totalQuantity
 }
